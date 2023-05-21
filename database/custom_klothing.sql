e65024890f9e55c29ec0613995fb448425cfc897
Akira Habane <119843352+AkiraHabane@users.noreply.github.com>
2023-05-21 22:09:05 +0800

    commit

---
+++ database/custom_klothing.sql
@@ -0,0 +1,444 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 21, 2023 at 12:32 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `custom_klothing`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `id` int(30) NOT NULL,
+  `client_id` int(30) NOT NULL,
+  `inventory_id` int(30) NOT NULL,
+  `price` double NOT NULL,
+  `quantity` int(30) NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(30) NOT NULL,
+  `category` varchar(250) NOT NULL,
+  `description` text DEFAULT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `category`, `description`, `status`, `date_created`) VALUES
+(5, 'Jackets', '&lt;p&gt;&lt;b&gt;Made with 100% Cotton.&lt;/b&gt;&lt;br&gt;&lt;/p&gt;', 1, '2023-05-20 15:03:54'),
+(6, 'Shirts', '&lt;p&gt;&lt;b&gt;Made with 100% Cotton.&lt;/b&gt;&lt;br&gt;&lt;/p&gt;', 1, '2023-05-20 15:04:56'),
+(7, 'Hoodies', '&lt;p&gt;&lt;b&gt;Made with 100% Cotton.&lt;/b&gt;&lt;br&gt;&lt;/p&gt;', 1, '2023-05-20 15:05:35');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `clients`
+--
+
+CREATE TABLE `clients` (
+  `id` int(30) NOT NULL,
+  `firstname` varchar(250) NOT NULL,
+  `lastname` varchar(250) NOT NULL,
+  `gender` varchar(20) NOT NULL,
+  `contact` varchar(15) NOT NULL,
+  `email` varchar(250) NOT NULL,
+  `password` text NOT NULL,
+  `default_delivery_address` text NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `clients`
+--
+
+INSERT INTO `clients` (`id`, `firstname`, `lastname`, `gender`, `contact`, `email`, `password`, `default_delivery_address`, `date_created`) VALUES
+(2, 'May', 'Cee', 'Female', '09207215112', 'maycee@gmail.com', '45cc96f18cff61eb19121c6e29c6bfdf', '1212 Tondo\r\nTondo', '2023-05-18 17:24:15'),
+(3, 'Ana', 'Castro', 'Male', '09123456789', 'anacastro@gmail.com', '3f8243ea4d1840079c9b95c0981c07df', '1212 Tondo\r\nManila', '2023-05-18 17:32:06'),
+(4, 'Van', 'Castro', 'Female', '09123456789', 'vancastro@gmail.com', '3f8243ea4d1840079c9b95c0981c07df', '1212 Tondo\r\nManila', '2023-05-21 16:15:45');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `inventory`
+--
+
+CREATE TABLE `inventory` (
+  `id` int(30) NOT NULL,
+  `product_id` int(30) NOT NULL,
+  `quantity` double NOT NULL,
+  `price` float NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `inventory`
+--
+
+INSERT INTO `inventory` (`id`, `product_id`, `quantity`, `price`, `date_created`, `date_updated`) VALUES
+(1, 1, 50, 2500, '2021-07-16 10:02:39', NULL),
+(5, 5, 100, 250, '2023-05-20 16:41:52', '2023-05-21 17:00:17'),
+(6, 6, 20, 150, '2023-05-20 19:27:26', '2023-05-20 23:07:51'),
+(7, 7, 100, 200, '2023-05-21 17:00:06', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(30) NOT NULL,
+  `client_id` int(30) NOT NULL,
+  `delivery_address` text NOT NULL,
+  `payment_method` varchar(100) NOT NULL,
+  `order_type` tinyint(1) NOT NULL COMMENT '1= pickup,2= deliver',
+  `amount` double NOT NULL,
+  `status` tinyint(2) NOT NULL DEFAULT 0,
+  `paid` tinyint(1) NOT NULL DEFAULT 0,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `client_id`, `delivery_address`, `payment_method`, `order_type`, `amount`, `status`, `paid`, `date_created`, `date_updated`) VALUES
+(26, 3, '1212 Tondo\r\nManila', 'cod', 2, 200, 0, 0, '2023-05-21 18:27:44', NULL),
+(27, 4, '1212 Tondo\r\nManila', 'Online Payment', 2, 600, 3, 1, '2023-05-21 18:29:18', '2023-05-21 18:29:56');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_list`
+--
+
+CREATE TABLE `order_list` (
+  `id` int(30) NOT NULL,
+  `order_id` int(30) NOT NULL,
+  `product_id` int(30) NOT NULL,
+  `quantity` int(30) NOT NULL,
+  `price` double NOT NULL,
+  `total` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `order_list`
+--
+
+INSERT INTO `order_list` (`id`, `order_id`, `product_id`, `quantity`, `price`, `total`) VALUES
+(5, 6, 5, 1, 250, 250),
+(6, 6, 6, 2, 150, 300),
+(7, 7, 5, 1, 250, 250),
+(10, 24, 5, 9, 250, 2250),
+(12, 26, 7, 1, 200, 200),
+(13, 27, 5, 1, 250, 250),
+(14, 27, 6, 1, 150, 150),
+(15, 27, 7, 1, 200, 200);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(30) NOT NULL,
+  `category_id` int(30) NOT NULL,
+  `sub_category_id` int(30) NOT NULL,
+  `product` varchar(250) NOT NULL,
+  `brandname` text NOT NULL,
+  `description` text NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `category_id`, `sub_category_id`, `product`, `brandname`, `description`, `status`, `date_created`) VALUES
+(5, 7, 9, 'Basic Hoodie', 'CustomKlothing', '&lt;p&gt;Made with 100% Cotton.&lt;/p&gt;', 1, '2023-05-20 16:40:35'),
+(6, 6, 13, 'DINO Shirt', 'CustomKlothing', '&lt;p&gt;Made with 100% Cotton.&lt;/p&gt;', 1, '2023-05-20 19:27:06'),
+(7, 6, 13, 'Game Over Shirt', 'CustomKlothing', '&lt;p&gt;&lt;b&gt;Made with 100% Cotton.&lt;/b&gt;&lt;/p&gt;', 1, '2023-05-21 16:54:29');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales`
+--
+
+CREATE TABLE `sales` (
+  `id` int(30) NOT NULL,
+  `order_id` int(30) NOT NULL,
+  `total_amount` double NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `sales`
+--
+
+INSERT INTO `sales` (`id`, `order_id`, `total_amount`, `date_created`) VALUES
+(10, 26, 200, '2023-05-21 18:27:45'),
+(11, 27, 600, '2023-05-21 18:29:20');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sub_categories`
+--
+
+CREATE TABLE `sub_categories` (
+  `id` int(30) NOT NULL,
+  `parent_id` int(30) NOT NULL,
+  `sub_category` varchar(250) NOT NULL,
+  `description` text NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT 1,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `sub_categories`
+--
+
+INSERT INTO `sub_categories` (`id`, `parent_id`, `sub_category`, `description`, `status`, `date_created`) VALUES
+(1, 1, 'Programming', '&lt;p&gt;Sample Sub Category&lt;/p&gt;', 1, '2021-07-16 09:10:44'),
+(2, 1, 'Grammar', '&lt;p&gt;Sample Sub 102&lt;/p&gt;', 1, '2021-07-16 09:11:05'),
+(3, 2, 'Literary', '&lt;p&gt;Sample Sub 103&lt;/p&gt;', 1, '2021-07-16 09:11:36'),
+(4, 2, 'Historical', '&lt;p&gt;Sample 104&lt;/p&gt;', 1, '2021-07-16 09:12:51'),
+(5, 3, 'Fantasy', '&lt;p&gt;Sample Sub 105&lt;/p&gt;', 1, '2021-07-16 09:13:28'),
+(6, 3, 'Action and Adventure', '&lt;p&gt;Sample Sub 106&lt;/p&gt;', 1, '2021-07-16 09:13:49'),
+(7, 4, 'Sub Cat 101', '&lt;p&gt;Sample Sub 107&lt;/p&gt;', 1, '2021-07-16 11:34:22'),
+(8, 0, 'Men', '', 1, '2023-05-20 16:30:28'),
+(9, 7, 'Unisex Hoodies', '', 1, '2023-05-20 16:30:59'),
+(10, 5, 'Unisex Jackets', '', 1, '2023-05-20 16:31:26'),
+(11, 6, 'For Men', '', 1, '2023-05-20 16:32:10'),
+(12, 6, 'For Women', '', 1, '2023-05-20 16:32:25'),
+(13, 6, 'Unisex', '', 1, '2023-05-20 16:33:36');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `system_info`
+--
+
+CREATE TABLE `system_info` (
+  `id` int(30) NOT NULL,
+  `meta_field` text NOT NULL,
+  `meta_value` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `system_info`
+--
+
+INSERT INTO `system_info` (`id`, `meta_field`, `meta_value`) VALUES
+(1, 'name', 'Custom Klothing'),
+(6, 'short_name', 'Custom Klothing'),
+(11, 'logo', 'uploads/1684662780_logo.png'),
+(13, 'user_avatar', 'uploads/user_avatar.jpg'),
+(14, 'cover', 'uploads/1684662300_banner.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(50) NOT NULL,
+  `firstname` varchar(250) NOT NULL,
+  `lastname` varchar(250) NOT NULL,
+  `username` text NOT NULL,
+  `password` text NOT NULL,
+  `avatar` text DEFAULT NULL,
+  `last_login` datetime DEFAULT NULL,
+  `type` tinyint(1) NOT NULL DEFAULT 0,
+  `date_added` datetime NOT NULL DEFAULT current_timestamp(),
+  `date_updated` datetime DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `username`, `password`, `avatar`, `last_login`, `type`, `date_added`, `date_updated`) VALUES
+(1, 'Custom', 'Klothing', 'ckadmin', 'eae7215f4f9d074f466b278852bf3d27', 'uploads/1684664280_1684664100_logo.png', NULL, 1, '2021-01-20 14:02:37', '2023-05-21 18:19:03'),
+(4, 'Cyber', 'Knights', 'ckadmin', '59b0c15503b7b9531e66752428c073bd', 'uploads/1684664100_logo.png', NULL, 0, '2021-06-19 08:36:09', '2023-05-21 18:15:58');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `clients`
+--
+ALTER TABLE `clients`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `inventory`
+--
+ALTER TABLE `inventory`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `order_list`
+--
+ALTER TABLE `order_list`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sales`
+--
+ALTER TABLE `sales`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sub_categories`
+--
+ALTER TABLE `sub_categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `system_info`
+--
+ALTER TABLE `system_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `clients`
+--
+ALTER TABLE `clients`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `inventory`
+--
+ALTER TABLE `inventory`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT for table `order_list`
+--
+ALTER TABLE `order_list`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `sales`
+--
+ALTER TABLE `sales`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `sub_categories`
+--
+ALTER TABLE `sub_categories`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `system_info`
+--
+ALTER TABLE `system_info`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
